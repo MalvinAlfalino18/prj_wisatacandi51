326d24eaae2f3d3e3478239fda0497918372133f
MalvinAlfalino18 <malvinalfalino18@mhs.mdp.ac.id>
2023-10-16 20:03:51 +0700

    init commit

---
+++ android/app/src/main/kotlin/com/example/prj_wisatacandi51/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.prj_wisatacandi51
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'wisata candi',
+      theme: ThemeData(),
+      home: const DetailScreen(),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
