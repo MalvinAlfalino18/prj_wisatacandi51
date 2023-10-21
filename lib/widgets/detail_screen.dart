766d19851c8407eb62f5bd83ff826730bd525046
malvin <malvinalfalino18@mhs.mdp.ac.id>
2023-10-21 18:36:59 +0700

    melanjutkan detail screen

--- lib/widgets/detail_screen.dart
+++ lib/widgets/detail_screen.dart
@@ -10,23 +10,80 @@ class DetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children : (
-         Stack(
-          children : [
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal:8),
-              child: Image.asset(
-                candi.imageAsset,
-                width: double.infinity,
-                height: 300,
-                fit: BoxFit.cover,
-              )
-            
+        children: [
+          Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.8),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset(
+                    candi.imageAsset,
+                    width: double.infinity,
+                    height: 300,
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ),
-          ]
-            )
-         ) 
-        )
-      )  
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.deepPurple.withOpacity(0.8),
+                      shape: BoxShape.circle),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.arrow_back),
+                  ),
+                ),
+              )
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              children: [
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      candi.name,
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.favorite_border))
+                  ],
+                ),
+                SizedBox(height: 16),
+                Row(
+                  children: [
+                    Icon(Icons.place, color: Colors.red),
+                    SizedBox(width: 8),
+                    const SizedBox(
+                      width: 70,
+                      child: Text(
+                        'Lokasi',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Text(': ${candi.location}')
+                  ],
+                ),
+                Row(
+                  children: [],
+                ),
+                Row(
+                  children: [],
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
